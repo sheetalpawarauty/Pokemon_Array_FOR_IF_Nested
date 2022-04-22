1381dc5804026ca6035de684212b390a574125fb
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-04-22 14:46:13 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let pokemonList = [
+    {name: 'pikachu', height: 4, types: ['field','fairy']},
+    {name: 'bulbasaur', height: 7, types: ['monster','grass']},
+    {name: 'paras', height: 3, types: ['bug','grass']},
+    {name: 'squirtle', height: 5, types: ['monster','water']},
+    {name: 'seel', height: 11, types: ['water','field']}
+  ];
+
+  
+  for(let i=0;pokemonList[i];i++) {
+    document.write(pokemonList[i].name+" "+"(height: "+pokemonList[i].height+")");
+    if(pokemonList[i].height>10){
+      document.write(" Wow! That's big!")
+    }
+    document.write("<br/>");
+  }
