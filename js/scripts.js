dd1ff7cf7c43d5e4d8bd32ece8d19248bebb1f6a
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-04-23 20:59:36 +0200

    Update scripts.js
    If applied, this commit will have <ul> tag placed out of the loop; hence showing it correctly in the web inspector too.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 let pokemonList = [
-    {name: 'pikachu', height: 4, types: ['field','fairy']},
-    {name: 'bulbasaur', height: 7, types: ['monster','grass']},
-    {name: 'paras', height: 3, types: ['bug','grass']},
-    {name: 'seel', height: 11, types: ['water','field']},
-    {name: 'squirtle', height: 5, types: ['monster','water']}
+    { name: 'pikachu', height: 4, types: ['field','fairy'] },
+    { name: 'bulbasaur', height: 7, types: ['monster','grass'] },
+    { name: 'paras', height: 3, types: ['bug','grass'] },
+    { name: 'seel', height: 11, types: ['water','field'] },
+    { name: 'squirtle', height: 5, types: ['monster','water'] }
   ];
 
   document.write("Pokemon Array List:");
-
+  document.write("<ul>");
   for(let i=0;i<pokemonList.length;i++) {
-    document.write("<ul>");
     document.write("<li>"+pokemonList[i].name+" "+"(height: "+pokemonList[i].height+")<\/li>");
-    if(pokemonList[i].height>10){
-      document.write(" Wow! That's big!")
+    if(pokemonList[i].height>10) {
+      document.write(" Wow! That's big!");
     }
-    document.write("<\/li><\/ul>");
+    document.write("<\/li>");
   }
+  document.write("<\/ul>");
