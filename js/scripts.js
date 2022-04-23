806d92e04415e3f96f5f72eb80d3f3e4e4c9b626
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-04-23 17:00:47 +0200

    Update scripts.js
    If applied, this will have the list of array items displayed as an unordered list!

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ let pokemonList = [
     {name: 'pikachu', height: 4, types: ['field','fairy']},
     {name: 'bulbasaur', height: 7, types: ['monster','grass']},
     {name: 'paras', height: 3, types: ['bug','grass']},
-    {name: 'squirtle', height: 5, types: ['monster','water']},
-    {name: 'seel', height: 11, types: ['water','field']}
+    {name: 'seel', height: 11, types: ['water','field']},
+    {name: 'squirtle', height: 5, types: ['monster','water']}
   ];
 
-  
+  document.write("Pokemon Array List:");
+
   for(let i=0;i<pokemonList.length;i++) {
-    document.write(pokemonList[i].name+" "+"(height: "+pokemonList[i].height+")");
+    document.write("<ul>");
+    document.write("<li>"+pokemonList[i].name+" "+"(height: "+pokemonList[i].height+")<\/li>");
     if(pokemonList[i].height>10){
       document.write(" Wow! That's big!")
     }
-    document.write("<br/>");
+    document.write("<\/li><\/ul>");
   }
